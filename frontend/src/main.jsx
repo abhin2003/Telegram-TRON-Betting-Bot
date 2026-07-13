14b5d83a854bfe6ebecfd994852ee7d29237406f
abhin2003 <unniskrishna3@gmail.com>
2026-07-13 18:08:26 +0530

    Remove TWA SDK completely

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import WebApp from '@twa-dev/sdk'
 import './index.css'
 import App from './App.jsx'
 
-if (WebApp && typeof WebApp.ready === 'function') {
-  WebApp.ready();
-} else if (window.Telegram && window.Telegram.WebApp) {
+if (window.Telegram && window.Telegram.WebApp) {
   window.Telegram.WebApp.ready();
 }
 

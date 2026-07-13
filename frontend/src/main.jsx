a169c923971e3612e7094800ba6579c0fb62fcee
abhin2003 <unniskrishna3@gmail.com>
2026-07-13 18:07:44 +0530

    Fix WebApp initialization crash

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,11 @@ import WebApp from '@twa-dev/sdk'
 import './index.css'
 import App from './App.jsx'
 
-WebApp.ready();
+if (WebApp && typeof WebApp.ready === 'function') {
+  WebApp.ready();
+} else if (window.Telegram && window.Telegram.WebApp) {
+  window.Telegram.WebApp.ready();
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
